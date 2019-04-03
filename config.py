536bdc311a6304cbc1e4b624c15e820dcbc7338e
Dima <random1k11@gmail.com>
2019-04-03 21:03:38 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+            'sqlite:///' + os.path.join(basedir, 'blog.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
