eb3ed0eaa964be7ed9b8b26d8033556edf82c9a6
Dima <random1k11@gmail.com>
2019-04-04 17:53:17 +0300

    Installed flask-bootstrap and added navbar in base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 from app import routes
 app.config.from_object(Config)
