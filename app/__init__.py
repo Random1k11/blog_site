bdcac4dd545d820eca4f10bc9530f1983e3dd26f
Dima <random1k11@gmail.com>
2019-04-06 22:22:44 +0300

    installed sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+from posts.blueprint import posts
+
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.register_blueprint(posts, url_prefix='/blog')
 
 
 from app import routes
