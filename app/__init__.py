aa8e550bc576e5ffcf61a5bc71374a2ab6158c36
Dima <random1k11@gmail.com>
2019-04-11 22:07:59 +0300

    Installed flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from posts.blueprint import posts
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 app.register_blueprint(posts, url_prefix='/blog')
+migrate = Migrate(app, db)
 
 
 from app import routes
