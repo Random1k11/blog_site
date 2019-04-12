ecc57f673d98709fa3851e34f57f0436c624d447
Dima <random1k11@gmail.com>
2019-04-12 19:32:15 +0300

    Created "Post" model in models.py

--- models.py
+++ models.py
@@ -11,3 +11,14 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    header = db.Column(db.String(140))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
